089c8a59f088d5612a7ee7989c51a79f9fadfa24
TokenBrice <me@tokenbrice.com>
2026-04-04 20:35:31 +0200

    docs(changelog): add changelog for 2026-03-25 to 2026-04-04

--- src/data/changelogs/index.ts
+++ src/data/changelogs/index.ts
@@ -1,8 +1,9 @@
 import type { ChangelogEntry } from "./types";
 
 import { entry as e20260324 } from "./2026-03-24";
+import { entry as e20260404 } from "./2026-04-04";
 
-const all: ChangelogEntry[] = [e20260324];
+const all: ChangelogEntry[] = [e20260324, e20260404];
 
 export const changelogs: ChangelogEntry[] = all.sort(
   (a, b) => b.dateRange.to.localeCompare(a.dateRange.to),
