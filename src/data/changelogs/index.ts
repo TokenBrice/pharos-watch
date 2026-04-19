1be98245a243116f49e2b851015b9b34e5bf38c9
tokenbrice <me@tokenbrice.com>
2026-04-19 11:23:45 +0200

    docs(changelog): replace 2026-04-17 entry with 2026-04-19 snapshot

--- src/data/changelogs/index.ts
+++ src/data/changelogs/index.ts
@@ -5,9 +5,9 @@ import { entry as e20260316 } from "./2026-03-16";
 import { entry as e20260324 } from "./2026-03-24";
 import { entry as e20260404 } from "./2026-04-04";
 import { entry as e20260412 } from "./2026-04-12";
-import { entry as e20260417 } from "./2026-04-17";
+import { entry as e20260419 } from "./2026-04-19";
 
-const all: ChangelogEntry[] = [e20260308, e20260316, e20260324, e20260404, e20260412, e20260417];
+const all: ChangelogEntry[] = [e20260308, e20260316, e20260324, e20260404, e20260412, e20260419];
 
 export const changelogs: ChangelogEntry[] = all.sort(
   (a, b) => b.dateRange.to.localeCompare(a.dateRange.to),
